473c90151f38c8f761ff02993ae3f8744b01491a
jayteealao <jayteealao@gmail.com>
2023-06-07 21:13:14 +0100

    Convert raw html strings returned by fts4 snippets to annotatedStrings

---
+++ app/src/main/java/com/jayteealao/trails/common/Constants.kt
@@ -0,0 +1,5 @@
+package com.jayteealao.trails.common
+
+const val ACCESSTOKEN = "access_token"
+const val CONSUMERKEY = "106656-15492d629f29f44e93a1b58"
+const val REDIRECTURI = "https://www.jayteealao.com"
--- app/src/main/java/com/jayteealao/trails/ui/pocket/components/Pocketitem.kt
+++ app/src/main/java/com/jayteealao/trails/ui/pocket/components/Pocketitem.kt
@@ -10,8 +10,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.core.text.HtmlCompat
+import androidx.core.text.toSpannable
+import com.jayteealao.trails.common.ext.toAnnotatedString
 import com.jayteealao.trails.data.local.database.PocketArticle
 import com.jayteealao.trails.data.local.database.PocketTuple
 import java.net.URL
@@ -22,6 +27,16 @@ fun PocketItem(
     modifier: Modifier = Modifier,
     onCLick: () -> Unit
 ) {
+
+    val parsedSnippet: AnnotatedString? = if (!article.snippet.isNullOrBlank()) {
+        HtmlCompat.fromHtml(
+            article.snippet,
+            HtmlCompat.FROM_HTML_MODE_LEGACY
+        ).toSpannable().toAnnotatedString(
+            Color.Black)
+    } else {
+        null
+    }
     Column(
         modifier = modifier
             .padding(
@@ -46,9 +61,9 @@ fun PocketItem(
             overflow = TextOverflow.Ellipsis
         )
         Spacer(modifier = Modifier.height(8.dp))
-        if (!article.snippet.isNullOrBlank()) {
+        if (!parsedSnippet.isNullOrBlank()) {
             Text(
-                text = article.snippet,
+                text = parsedSnippet,
                 style = MaterialTheme.typography.bodyMedium,
                 maxLines = 3,
                 overflow = TextOverflow.Ellipsis
